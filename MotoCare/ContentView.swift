2d0b6e9146f33a75913490988945733bff795032
Jose Manuel Dominguez Garcia <josemanueldg02@gmail.com>
2026-07-10 16:32:51 +0200

    Initial Commit

---
+++ MotoCare/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MotoCare
+//
+//  Created by Jose Manuel Dominguez Garcia on 10/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MotoCare/MotoCareApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotoCareApp.swift
+//  MotoCare
+//
+//  Created by Jose Manuel Dominguez Garcia on 10/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MotoCareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
